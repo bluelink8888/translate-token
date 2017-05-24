0ab3e2bf7dc44a315e49ab8e858ec134ca46c0db
幼暐 洪 <bluelink8888@gmail.com>
2017-05-25 00:13:50 +0800

    finish bp function

--- src/main/java/org/yuwei/google_translate/Token.java
+++ src/main/java/org/yuwei/google_translate/Token.java
@@ -1,24 +1,86 @@
 package org.yuwei.google_translate;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Token {
   
   public Object yp(Object a){
+    
     return a;
   }
   
+  /*
+  zp = function (a, b) {
+        for (var c = 0; c < b.length - 2; c += 3) {
+            var d = b.charAt(c + 2)
+                , d = "a" <= d ? d.charCodeAt(0) - 87 : Number(d)
+                , d = "+" == b.charAt(c + 1) ? a >>> d : a << d;
+            a = "+" == b.charAt(c) ? a + d & 4294967295 : a ^ d
+        }
+        return a
+    };
+  */
   // b = +-a^+6
   public long zp(long a, String b){
     
+    long d = 0;
     for(int i = 0; i<b.length()-2; i +=3){
       String c = "" + b.charAt(i+2);
-      long d = 0;
-      if("a".codePointAt(0)<= c.charAt(0)){
-      }
-      d = ("a".codePointAt(0)<= c.charAt(0)) ? c.codePointAt(0) -87 : Long.parseLong(c);
+      d = b.charAt(i+2);
+      d = ("a".codePointAt(0)<= d ? c.codePointAt(0) -87 : Long.parseLong(c));
+      d = ("+".equals(b.charAt(i+1))) ? a>>>d : a<<d;
       a = ("+".equals(b.charAt(i))) ? a + d & 4294967295L : a ^ d;
     }
     
     return a;
+  }
+  
+  public void bp(String t){
+    
+    List<Integer> f = new LinkedList<Integer>();
+    
+    for(int i = 0; i < t.length();i++){
+      int l = t.codePointAt(i);
+      
+      if(128 > l){
+        f.add(l);
+      }else{
+        if(2048 > l){
+          f.add(l >> 6 |192);
+        }else{
+          if (55296 == (l & 64512) && i + 1 < t.length() && 56320 == (t.codePointAt(i+1) & 64512)){
+            l = 65536 + ((l & 1023) << 10) + (t.codePointAt(++i) & 1023);
+            f.add(l>>18 |240);
+            f.add(l >> 12 & 63 | 128);
+          }else{
+            f.add(l >> 12 | 224);
+            f.add(l >> 6 & 63 | 128);
+            f.add(l & 63 | 128);
+          }
+        }
+      }
+    }
+    
+    long a = 0;
+    
+    for(int i = 0; i < f.size(); i++){
+      a += Long.parseLong(f.get(i).toString());
+      a = this.zp(a, "+-a^+6");
+    }
+    
+    a = this.zp(a, "+-3^+b+-f");
+    
+    a %= 1E6;
+    
+    System.out.println(a);
+  }
+  
+  
+  public static void main(String[] args){
+    Token token = new Token();
+    token.bp("test");
+    System.out.println("116 zp : " + token.zp(116, "+-a^+6"));
   }
   
 }
